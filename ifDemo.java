2ac5821427a2b1e1eec20156f59946bf7a84eff5
Paul <jamespauloconnor@gmail.com>
2016-05-11 16:53:15 -0400

    Added if statement

---
+++ ifDemo.java
@@ -0,0 +1,11 @@
+class ifDemo{
+  public static void main(String args[]){
+    int num = 7;
+    
+    if (num == 7){
+      System.out.println("It's your lucky day!");
+    }else{
+      System.out.println("Sorry, dude. You're not lucky!");
+    }
+  }
+}
